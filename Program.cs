cd6a6003f1130b4c01c001c087d6cbc6f79f61b0
nikita <fireshape360@inbox.ru>
2022-09-20 20:32:33 +0300

    Added input and arrays

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿/*Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длинна которых <= 3 символа. Первоначальный массив можно 
 ввести с клавиатуры или задать со старта*/
 
+Console.WriteLine("Задайте размер массива");
+
+bool isNumber = int.TryParse(Console.ReadLine(), out int m);
+if (isNumber == false)
+{
+    Console.WriteLine("Неверные данные");
+    return;
+}
+
+string[] array = new string[m];
+
+string[] secondArray = new string[m];
+
